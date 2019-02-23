cf2875b8181911d92f11a0fbec7f0b1df6b7747b
Jonas Bieri <jonas.bieri@gmail.com>
2019-02-23 14:44:16 +0000

    Add comments

--- user-config.py
+++ user-config.py
@@ -1,5 +1,7 @@
 mylang = "wikidata"
 family = "wikidata"
 usernames["wikidata"]["wikidata"] = "DataKarate"
+#How to create file user-password.py: see https://www.mediawiki.org/wiki/Manual:Pywikibot/BotPasswords, e.g. 
+#(u'DataKarate', BotPassword(u'DataKarateBot', u'MySuperSecretPassword!1'))
 password_file = "user-password.py"
 put_throttle = 1
