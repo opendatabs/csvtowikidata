de06e40b8e3272785ab74b53a5a0d417b39c92b0
Jonas Bieri <jonas.bieri@gmail.com>
2019-02-23 16:36:39 +0000

    Enhance

--- test.py
+++ test.py
@@ -19,8 +19,8 @@ time_prop_id = 'P585'
 url_prop_id = 'P854'
 retrieved_prop_id = 'P813'
 source_url = 'https://www.statistik.bs.ch/dam/jcr:2d711b31-d8c9-4f5d-9151-55ff4ef4028d/t01-1-16.xlsx'
-#item_id = "Q4115189" #wikidata sandbox
-item_id = "Q809909" #Basel-Klybeck
+item_id = "Q4115189" #wikidata sandbox
+#item_id = "Q809909" #Basel-Klybeck
 
 # connect to WikiData
 site = pywikibot.Site("wikidata", "wikidata")
@@ -29,32 +29,30 @@ item = pywikibot.ItemPage(repo, item_id)
 item.get()
 #print(item.claims)
 
-year_list = range(2018, 2018)
-#for year in year_list:
-year = 2018
-population_claim = existing_claim_from_year(item, year)
-if (population_claim is None):
-    
-    population_value = 7293
-    population_claim = pywikibot.Claim(repo, population_prop_id)
-    population_claim.setTarget(pywikibot.WbQuantity(amount=population_value, site=site))
-    item.addClaim(population_claim)
-    
-    timeQualifier = pywikibot.Claim(repo, time_prop_id)
-    yearObj = pywikibot.WbTime(year=year)
-    timeQualifier.setTarget(yearObj)
-    population_claim.addQualifier(timeQualifier)
-    
-    retrievedQualifier = pywikibot.Claim(repo, retrieved_prop_id)
-    today = pywikibot.WbTime(year=2019, month=2, day=23)
-    retrievedQualifier.setTarget(today)
-    population_claim.addQualifier(retrievedQualifier)
-    
-    source = pywikibot.Claim(repo, url_prop_id)
-    source.setTarget(source_url)
-    population_claim.addSource(source)
-    
-    population_claim.changeRank("preferred")
-else:
-    print ("Population claim already exists on %s for year %d, skipping") % (item_id, year)
+year_list = range(2018, 2019)
+for year in year_list:
+#year = 2018
+    population_claim = existing_claim_from_year(item, year)
+    if (population_claim is None):
         
+        population_value = 7293
+        population_claim = pywikibot.Claim(repo, population_prop_id)
+        population_claim.setTarget(pywikibot.WbQuantity(amount=population_value, site=site))
+        item.addClaim(population_claim)
+        
+        timeQualifier = pywikibot.Claim(repo, time_prop_id)
+        yearObj = pywikibot.WbTime(year=year, month=12, day=31)
+        timeQualifier.setTarget(yearObj)
+        population_claim.addQualifier(timeQualifier)
+        
+        source = pywikibot.Claim(repo, url_prop_id)
+        source.setTarget(source_url)
+        retrieved = pywikibot.Claim(repo, retrieved_prop_id)
+        today = pywikibot.WbTime(year=2019, month=2, day=23)
+        retrieved.setTarget(today)
+        population_claim.addSources([source, retrieved])
+    
+        population_claim.changeRank("preferred")
+    else:
+        print ("Population claim already exists on %s for year %d, skipping") % (item_id, year)
+            
